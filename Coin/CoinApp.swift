187aa5d9d3d32a7670f4d2abbd1ed2ef09fef92c
ha-ny <hany01.dev@gmail.com>
2023-12-26 10:18:22 +0900

    Initial Commit

---
+++ Coin/CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinApp.swift
+//  Coin
+//
+//  Created by 김하은 on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coin
+//
+//  Created by 김하은 on 12/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
