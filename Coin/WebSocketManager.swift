d27f4ce6c03bd012cc5a7331bb67424f3c933bfb
ha-ny <hany01.dev@gmail.com>
2023-12-26 10:25:38 +0900

    setting

---
+++ Coin/WebSocketManager.swift
@@ -0,0 +1,15 @@
+//
+//  WebSocketManager.swift
+//  Coin
+//
+//  Created by 김하은 on 12/26/23.
+//
+
+import Foundation
+
+final class WebSocketManager {
+    static let shared = WebSocketManager()
+    
+    private init() { }
+    
+}
